dae0ff01dc75bd04d1b7b981ccbf047fc88c7b91
Ben Chatelain <benchatelain@gmail.com>
2014-02-26 20:42:15 -0700

    Initial Commit

---
+++ SubspecUpdateIssue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SubspecUpdateIssue
+//
+//  Created by Ben Chatelain (KP) on 2/26/14.
+//  Copyright (c) 2014 @phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SubspecUpdateIssue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SubspecUpdateIssue
+//
+//  Created by Ben Chatelain (KP) on 2/26/14.
+//  Copyright (c) 2014 @phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
